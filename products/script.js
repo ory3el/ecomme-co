12642f0f3e9e7753fcc3fae74c63079dec7dd3c5
Ryeel <legendaryfirelfg@gmail.com>
2026-06-13 18:32:26 -0300

    Fix onclick attribute syntax in button element

--- products/script.js
+++ products/script.js
@@ -157,7 +157,7 @@ function renderProducts() {
           <div class="pimg">${p.emoji}</div>
           <div class="pbadges">${badgeH}</div>
           <button class="pwish-btn ${inW ? 'on' : ''}"
-                  onclick="event.stopPropagation();toggleFav(${p.id}) renderProducts()"
+                  onclick="event.stopPropagation();toggleFav(${p.id}); renderProducts();"
                   title="${inW ? 'Remover dos favoritos' : 'Adicionar aos favoritos'}">
             <svg viewBox="0 0 24 24"><path d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z"/></svg>
           </button>
