f20d04ad06e94e79867f25ab2897924403c5826c
Ryeel <legendaryfirelfg@gmail.com>
2026-06-13 18:44:55 -0300

    Replace heart emoji with SVG icon in button

--- products/script.js
+++ products/script.js
@@ -251,7 +251,7 @@ function updateCart() {
         </svg>
       </button>
       <button class="cart-item-towish" onclick="moveFromCartToFav(${item.id})" title="Mover para Lista de Desejos">
-        ❤️
+        <svg viewBox="0 0 24 24"><path d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z"/></svg>
       </button>
     </div>`).join('');
   renderProducts();
