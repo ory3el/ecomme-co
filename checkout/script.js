429e7fb791b7687b0f4b8ddfca5109941cf96be4
Gabriel <gabrielkosteski1@gmail.com>
2026-07-16 19:34:09 -0300

    Update script.js

--- checkout/script.js
+++ checkout/script.js
@@ -19,6 +19,7 @@ mqDark.addEventListener('change', checkTheme);
 /* ─── SUPABASE ──────────────────────────────────────────────────────── */
 const SUPABASE_URL = "https://cedrpcezoaqaeivrfuxn.supabase.co";
 const SUPABASE_ANON_KEY = "sb_publishable_mgumCH-bhkDOZfzqaMjKzQ_OwPVESs0";
+const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 let userId = null;
 
 // ── STATE ──────────────────────────────────────────────
