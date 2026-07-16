29bfc1155690ba19a14a5c1c987b7c2c9c431795
Gabriel <gabrielkosteski1@gmail.com>
2026-07-16 20:05:53 -0300

    Update script.js

--- checkout/script.js
+++ checkout/script.js
@@ -202,7 +202,7 @@ function updateCart() {
   const count = cart.reduce((s, i) => s + i.qty, 0);
   
   //$('cartBadge').textContent = count;
-  $('cartBadge').style.display = count > 0 ? 'flex' : 'none';
+  //$('cartBadge').style.display = count > 0 ? 'flex' : 'none';
   $('cartCount').textContent = `(${count})`;
   $('cartSub').textContent   = fmt(total);
   $('cartTotal').textContent = fmt(total);
@@ -301,7 +301,7 @@ function updateFav() {
   const total = fav.reduce((s, i) => s + i.price * i.qty, 0);
   const count = fav.reduce((s, i) => s + i.qty, 0);
   //$('wishBadge').textContent = count;
-  $('wishBadge').style.display = count > 0 ? 'flex' : 'none';
+  //$('wishBadge').style.display = count > 0 ? 'flex' : 'none';
   $('favCount').textContent = `(${count})`;
   $('favTotal').textContent = fmt(total);
 
