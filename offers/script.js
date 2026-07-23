e90bd8aef0d6dae1700345378a480ba5d577e6cd
Gabriel <gabrielkosteski1@gmail.com>
2026-07-23 18:43:23 -0300

    Update script.js

--- offers/script.js
+++ offers/script.js
@@ -594,7 +594,7 @@ function renderOffers() {
           <div class="oc-img">${o.emoji}</div>
           <div class="oc-disc-badge">-${o.discount}%<small>OFF</small></div>
           ${typeBadge}
-          <button class="oc-wish ${inW ? 'on' : ''}" onclick="event.stopPropagation(); toggleWish('${o.id}')">
+          <button class="oc-wish ${inW ? 'on' : ''}" onclick="event.stopPropagation(); toggleFav(${o.id})">
             <svg viewBox="0 0 24 24"><path d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z"/></svg>
           </button>
           <div class="oc-actions">
