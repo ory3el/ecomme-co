b32ca0eda70272a96883acfad368e1453f609bbd
Gabriel <gabrielkosteski1@gmail.com>
2026-07-30 22:16:35 -0300

    Update script.js

--- sellers/register/script.js
+++ sellers/register/script.js
@@ -145,7 +145,6 @@ window.addEventListener('DOMContentLoaded', async () => {
         headerImage.style.objectFit = "cover";
       }
     }
-  }
 });
 
 /* ============================================================ MASK HELPERS */
