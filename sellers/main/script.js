733517fa4f59ab7dbe37af5fad43279412ad84fb
Gabriel <gabrielkosteski1@gmail.com>
2026-08-11 15:23:12 -0300

    Update script.js

--- sellers/main/script.js
+++ sellers/main/script.js
@@ -781,8 +781,8 @@ function subscribeToStoreStatus() {
     .subscribe();
 }
 
-async function saveConfig(loja) {
-  if (loja) event.preventDefault();
+async function saveConfig() {
+  event.preventDefault();
 
   if (!userId) {
     console.error("Usuário não autenticado.");
@@ -793,7 +793,12 @@ async function saveConfig(loja) {
   const cfgStoreDescOK = cfgStoreDesc.value.length >= 10;
   const cfgStoreEmailOK = cfgStoreEmail.value.length >= 3;
   const cfgStorePhoneOK = cfgStorePhone.value.length >= 10;
-      
+
+  const storeCnpjCpfOK = storeCnpjCpf.value.length >= 11;
+  const storeLegalNameOK = storeLegalName.value.length >= 3;
+  const storeTradeNameOK = storeTradeName.value.length >= 3;
+  const storeTaxRegimeOK = storeTaxRegime.value.length >= 8;
+  
   if (!cfgStoreNameOK) {
     showAlert('Para salvar e atualizar as informações da sua loja, complete todos os campos marcados com * (asterisco).', 'Complete os campos obrigatórios.', 'ℹ️')
     console.error("Erro ao salvar. Complete o campo Nome da Loja.");
@@ -817,68 +822,8 @@ async function saveConfig(loja) {
     console.error("Erro ao salvar. Complete o campo WhatsApp / Telefone.");
     return;
   }
-  
-  const btnSalvar = document.getElementById('btn-save-config');
-  const textoOriginal = btnSalvar.innerText;
-  
-  if (btnSalvar) {
-    btnSalvar.innerText = 'Salvando...';
-    btnSalvar.disabled = true;
-  }
-  
-  try {
-    const dadosParaSalvar = {
-      nome_loja: cfgStoreName.value,
-      slogan: cfgStoreSlogan.value,
-      descricao: cfgStoreDesc.value,
-      email: cfgStoreEmail.value,
-      telefone: cfgStorePhone.value,
 
-      // Exemplos de campos que você pode adicionar futuramente:
-      // cor_primaria: document.getElementById('input-cor-tema').value,
-      // cep_origem: document.getElementById('input-cep').value
-    };
-    
-    const { data, error } = await supabaseClient
-      .from('lojas')
-      .update(dadosParaSalvar)
-      .eq('user_id', userId)
-      .select();
-
-    if (error) {
-      throw error;
-    }
-    console.log("Configurações salvas com sucesso!", data);
-    toast("As configurações foram salvas com sucesso!");
-    
-    renderStoreState(data[0].status, data[0].slug_url, data[0]);
-
-  } catch (err) {
-    console.error("Erro ao salvar configurações:", err.message);
-    toast("Ocorreu um erro ao salvar. Verifique sua conexão e tente novamente.");
-  } finally {
-
-    if (btnSalvar) {
-      btnSalvar.innerText = textoOriginal;
-      btnSalvar.disabled = false;
-    }
-  }
-}
-
-/* ------------------------------------ */
-
-async function saveConfig(dadosLegais) {
-  if (dadosLegais) event.preventDefault();
-
-  if (!userId) {
-    console.error("Usuário não autenticado.");
-    return;
-  }
-
-  const storeCnpjCpfOK = storeCnpjCpf.value.length >= 3;
-  const storeLegalNameOK = storeLegalName.value.length >= 10;
-  const storeTradeNameOK = storeTradeName.value.length >= 3;
-  const storeTaxRegimeOK = storeTaxRegime.value.length >= 10;
+  /* ------------------------------------------------------------ */
       
   if (!storeCnpjCpfOK) {
     showAlert('Para salvar e atualizar as informações da sua loja, complete todos os campos marcados com * (asterisco).', 'Complete os campos obrigatórios.', 'ℹ️')
@@ -914,11 +859,17 @@ async function saveConfig(dadosLegais) {
   
   try {
     const dadosParaSalvar = {
+      nome_loja: cfgStoreName.value,
+      slogan: cfgStoreSlogan.value,
+      descricao: cfgStoreDesc.value,
+      email: cfgStoreEmail.value,
+      telefone: cfgStorePhone.value,
+
       documento: storeCnpjCpf.value,
       nome_razao: storeLegalName.value,
       nome_fantasia: storeTradeName.value,
       regime_tributario: storeTaxRegime.value,
-
+      
       // Exemplos de campos que você pode adicionar futuramente:
       // cor_primaria: document.getElementById('input-cor-tema').value,
       // cep_origem: document.getElementById('input-cep').value
