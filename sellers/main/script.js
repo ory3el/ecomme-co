0e458c40f035d8523e8f17ef1777a61c052dd5fc
Gabriel <gabrielkosteski1@gmail.com>
2026-08-13 19:33:45 -0300

    Update script.js

--- sellers/main/script.js
+++ sellers/main/script.js
@@ -1245,8 +1245,8 @@ document.querySelectorAll('.inp, .ta, .sel').forEach(elemento => {
   });
 });
 
-// const navButtons = document.querySelectorAll('.nav-btn, .cfg-nav-item, .config-tab');
-const navButtons = document.querySelectorAll('.nav-btn');
+// const navButtons = document.querySelectorAll('.nav-btn, mob-btn, .cfg-nav-item, .config-tab');
+const navButtons = document.querySelectorAll('.nav-btn, mob-btn');
 
 navButtons.forEach(button => {
     button.addEventListener('click', (event) => {
