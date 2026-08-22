4a3a60850edac93a1d18b50e4c0b584145fff438
Gabriel <gabrielkosteski1@gmail.com>
2026-08-21 21:16:03 -0300

    Update script.js

--- sellers/main/script.js
+++ sellers/main/script.js
@@ -771,7 +771,7 @@ function updatePreview() {
         currentPreviewFile = mainImageFile;
       }
 
-      previewEl.innerHTML = `<img src="${currentPreviewBlobUrl}" style="position:absolute; top:0; left:0; width:100%; height:100%; object-fit:cover; border-radius:inherit;" alt="Preview">`;
+      previewEl.innerHTML = `<img src="${currentPreviewBlobUrl}" style="position:absolute; top:0; left:0; width:100%; /*height:100%;*/ object-fit:cover; border-radius:inherit;" alt="Preview">`;
     } else if (!isEditing) {
       if (previewEl.tagName.toLowerCase() !== 'img') {
         previewEl.innerHTML = `<div class="fs24" style="display:flex; align-items:center; justify-content:center; width:100%; height:100%;">${em}</div>`;
