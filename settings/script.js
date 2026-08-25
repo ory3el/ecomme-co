62af3507660f74669fab2a1a89ee8b0ad753281a
Gabriel <gabrielkosteski1@gmail.com>
2026-08-25 16:32:54 -0300

    Update script.js

--- settings/script.js
+++ settings/script.js
@@ -460,11 +460,16 @@ async function removePhoto(event) {
   }
 }
 
-// FUNÇÃO DE LOGOUT
+// LOGOUT
+const waitt = (ms) => new Promise(resolve => setTimeout(resolve, ms));
 async function doLogout() { 
   toast('Saindo da conta... 👋', 'info'); 
-  await supabaseClient.auth.signOut();
-  goToLogin()
+  await supabaseClient.auth.signOut({scope: 'local'});
+  toast('Conta deslogada, recarregando a página.', 'info');
+  
+  closeAcc();
+  await waitt(1000);
+  window.location.reload()
 }
 
 // ── SUPABASE: ADDRESSES ───────────────────────────
