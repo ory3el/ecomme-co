8006434b11e05d71d2ee5dfe1f11daef3dc919f5
Gabriel <gabrielkosteski1@gmail.com>
2026-08-25 16:36:33 -0300

    Update script.js

--- settings/script.js
+++ settings/script.js
@@ -465,9 +465,8 @@ const waitt = (ms) => new Promise(resolve => setTimeout(resolve, ms));
 async function doLogout() { 
   toast('Saindo da conta... 👋', 'info'); 
   await supabaseClient.auth.signOut({scope: 'local'});
-  toast('Conta deslogada, recarregando a página.', 'info');
+  toast('Você saiu da conta, recarregando a página.', 'info');
   
-  closeAcc();
   await waitt(1000);
   window.location.reload()
 }
