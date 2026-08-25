dfe6e1355fef7fbaa15d30adfb4b6ac07f520064
Gabriel <gabrielkosteski1@gmail.com>
2026-08-25 16:38:49 -0300

    Update script.js

--- about-us/script.js
+++ about-us/script.js
@@ -719,8 +719,13 @@ updateCart();
 syncToSupabase();
 
 // LOGOUT
+const waitt = (ms) => new Promise(resolve => setTimeout(resolve, ms));
 async function doLogout() { 
   toast('Saindo da conta... 👋', 'info'); 
-  await supabaseClient.auth.signOut();
-  buttonLink('/login')
+  await supabaseClient.auth.signOut({scope: 'local'});
+  toast('Você saiu da conta, recarregando a página.', 'info');
+
+  closeAcc();
+  await waitt(1000);
+  window.location.reload();
 }
