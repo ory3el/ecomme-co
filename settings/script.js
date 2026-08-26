517dc91b01cf6b99a3ee521cb41ddbe7349811c7
Gabriel <gabrielkosteski1@gmail.com>
2026-08-26 10:40:36 -0300

    Update script.js

--- settings/script.js
+++ settings/script.js
@@ -1,11 +1,11 @@
 function showConsoleWarning() {
   console.log(
     '%cAVISO',
-    'background: rgba(255,0,0,0.5); color: #fff; font-size: 20px; font-weight: bold; padding: 2px 4px; border-radius: 15px;'
+    'background: rgba(255,255,255,0.9); color: red; font-size: 20px; font-weight: bold; padding: 2px 4px; border-radius: 15px;'
   );
   console.log(
     '%cO uso deste console pode permitir que invasores falsifiquem sua identidade para roubar informações por meio de um ataque chamado Self-XSS.\nNão insira nem cole códigos que você não conheça.',
-    'color: #fff; background: rgba(255,0,0,0.5); font-size: 18px; line-height: 1.5; padding: 2px;'
+    'color: #fff; background: rgba(255,0,0,0.9); font-size: 18px; line-height: 1.5; padding: 2px;'
   );
 }
 showConsoleWarning();
