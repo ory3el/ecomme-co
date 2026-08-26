b9aebe94341b7aa1ab696dbc7d1550236094df35
Gabriel <gabrielkosteski1@gmail.com>
2026-08-26 10:34:18 -0300

    Update script.js

--- settings/script.js
+++ settings/script.js
@@ -1,3 +1,15 @@
+function showConsoleWarning() {
+  console.log(
+    '%cAVISO',
+    'background: yellow; color: red; font-size: 20px; font-weight: bold; padding: 2px 4px;'
+  );
+  console.log(
+    '%cO uso deste console pode permitir que invasores falsifiquem sua identidade para roubar informações por meio de um ataque chamado Self-XSS.\nNão insira nem cole códigos que você não conheça.',
+    'color: #111; font-size: 16px; line-height: 1.5;'
+  );
+}
+showConsoleWarning();
+
 // ── NAV LINKS ──────────────────────────────────────
 function buttonLink(url) {
   window.location.href = url;
