d0869365664dd0ce0d3225114290c94e14d532df
Gabriel <gabrielkosteski1@gmail.com>
2026-08-28 17:48:35 -0300

    Update script.js

--- categories/script.js
+++ categories/script.js
@@ -34,12 +34,6 @@ function injectPrefetch(url) {
 
 let products = [];
 
-/* ─── SUPABASE ──────────────────────────────────────────────────────── */
-const SUPABASE_URL = "https://cedrpcezoaqaeivrfuxn.supabase.co";
-const SUPABASE_ANON_KEY = "sb_publishable_mgumCH-bhkDOZfzqaMjKzQ_OwPVESs0";
-const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
-let userId = null;
-
 // EXECUTE DATABASE
 window.addEventListener('DOMContentLoaded', async () => {
   const loginBtn = document.getElementById('authLoginBtn');
