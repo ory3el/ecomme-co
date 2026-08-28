61695b469df2a233a0347d76ba89c22c007bb759
Gabriel <gabrielkosteski1@gmail.com>
2026-08-28 18:02:24 -0300

    Update script.js

--- sellers/script.js
+++ sellers/script.js
@@ -34,12 +34,6 @@ function toast(msg,type='ok'){
   t._t=setTimeout(()=>t.classList.remove('on'),3000);
 }
 
-/* ─── SUPABASE ──────────────────────────────────────────────────────── */
-const SUPABASE_URL = "https://cedrpcezoaqaeivrfuxn.supabase.co";
-const SUPABASE_ANON_KEY = "sb_publishable_mgumCH-bhkDOZfzqaMjKzQ_OwPVESs0";
-const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
-let userId = null;
-
 // EXECUTE DATABASE
 window.addEventListener('DOMContentLoaded', async () => {
   const loginBtn = document.getElementById('authLoginBtn');
