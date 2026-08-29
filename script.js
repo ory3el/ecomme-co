a55c178145b7fa0f147d7967a6d0fcfbc41e6e75
Gabriel <gabrielkosteski1@gmail.com>
2026-08-28 21:05:26 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1061,6 +1061,7 @@ supabaseClient.auth.onAuthStateChange(
 );
 
 // LOGOUT
+const waitt = (ms) => new Promise(resolve => setTimeout(resolve, ms));
 async function doLogout() {
   toast('Saindo da conta... 👋', 'info');
   const localSessionId = localStorage.getItem('local_session_id');
