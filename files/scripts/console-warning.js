604b2a1beb13f5e26563c6ea4d2bf5c0026890af
Ryeel <legendaryfirelfg@gmail.com>
2026-09-03 08:48:21 -0300

    Update console-warning.js

--- files/scripts/console-warning.js
+++ files/scripts/console-warning.js
@@ -1,11 +1,11 @@
 function showConsoleWarning() {
   console.log(
     '%cAVISO',
-    'background: #fff; color: red; font-size: 20px; font-weight: bold; padding: 2px 4px; border-radius: 5px;'
+    'background: var(--white); color: red; font-size: 20px; font-weight: bold; padding: 2px 4px; border-radius: 5px;'
   );
   console.log(
     '%cO uso deste console pode permitir que invasores falsifiquem sua identidade para roubar informações por meio de um ataque chamado Self-XSS.\nNão insira nem cole códigos que você não conheça.',
-    'color: red; background: rgba(255,0,0,0.15); font-size: 18px; line-height: 1.5; padding: 2px; font-weight: 600; border-radius: 5px; '
+    'color: red; background: var(--glass-border); font-size: 18px; line-height: 1.5; padding: 2px; font-weight: 600; border-radius: 5px; '
   );
 }
-showConsoleWarning()
+showConsoleWarning();
