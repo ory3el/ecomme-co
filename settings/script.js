1ed32576dea2789ce7667d4f2294e50ea8c53d66
Gabriel <gabrielkosteski1@gmail.com>
2026-09-07 19:30:55 -0300

    Update script.js

--- settings/script.js
+++ settings/script.js
@@ -1,3 +1,5 @@
+const $ = id => document.getElementById(id);
+
 /* -- THEME --------------------------------------------------------- */
 function systemPrefersDark() {
   return window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
