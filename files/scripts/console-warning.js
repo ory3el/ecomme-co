da444800d1f0067e73d3f70e02464068b3ce77db
Gabriel <gabrielkosteski1@gmail.com>
2026-09-19 21:09:14 -0300

    Update console-warning.js

--- files/scripts/console-warning.js
+++ files/scripts/console-warning.js
@@ -9,14 +9,20 @@ function showConsoleWarning() {
   );*/
 
   console.log(
-    "%cPERIGO!"
-    "color: #ff0000; font-size: 50px; font-weight: bold; font-family: sans-serif; text-shadow: 2px 2px 0px #000;"
-    "%cEste é um recurso de navegador voltado exclusivamente para desenvolvedores."
-    "font-size: 16px; font-family: sans-serif; margin-top: 10px;"
-    "%cSe alguém disse para você copiar e colar um código aqui para ativar um recurso, ganhar descontos ou 'hackear' o sistema, é um golpe (ataque Self-XSS)."
+    "%cPERIGO!", 
+    "color: #ff0000; font-size: 40px; font-weight: bold; font-family: sans-serif; text-shadow: 2px 2px 0px #000;"
+  );
+  console.log(
+    "%cEste é um recurso de navegador voltado exclusivamente para desenvolvedores.", 
+    "font-size: 16px; font-weight: bold; font-family: sans-serif; margin-top: 10px;"
+  );
+  console.log(
+    "%cSe alguém disse para você copiar e colar um código aqui para ativar um recurso, ganhar descontos ou 'hackear' o sistema, é um golpe (ataque Self-XSS).", 
     "font-size: 16px; font-weight: bold; color: #ffcc00; font-family: sans-serif; margin-top: 10px;"
-    "%cColar qualquer código aqui dará aos invasores acesso total à sua conta, permitindo o roubo de dados pessoais, compras indevidas e acesso aos seus pagamentos."
-    "font-size: 16px; color: #ff5555; font-family: sans-serif; margin-top: 10px;"
+  );
+  console.log(
+    "%cColar qualquer código aqui dará aos invasores acesso total à sua conta, permitindo o roubo de dados pessoais, compras indevidas e acesso aos seus pagamentos.", 
+    "font-size: 16px; font-weight: bold; color: #ff5555; font-family: sans-serif; margin-top: 10px;"
   );
 }
 showConsoleWarning();
